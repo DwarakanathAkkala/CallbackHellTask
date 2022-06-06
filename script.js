ef0cee229d267dbbdc962fe179a3a56716352dde
DwarakanathAkkala <37907049+DwarakanathAkkala@users.noreply.github.com>
2022-06-07 00:23:13 +0530

    Task Upload

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+    document.querySelector(".container").innerHTML=`<h1>10</h1>`;
+    setTimeout(() => {
+        document.querySelector(".container").innerHTML=`<h1>9</h1>`;
+        setTimeout(() => {
+            document.querySelector(".container").innerHTML=`<h1>8</h1>`;
+            setTimeout(() => {
+                document.querySelector(".container").innerHTML=`<h1>7</h1>`;
+                setTimeout(() => {
+                    document.querySelector(".container").innerHTML=`<h1>6</h1>`;
+                    setTimeout(() => {
+                        document.querySelector(".container").innerHTML=`<h1>5</h1>`;
+                        setTimeout(() => {
+                            document.querySelector(".container").innerHTML=`<h1>4</h1>`;
+                            setTimeout(() => {
+                                document.querySelector(".container").innerHTML=`<h1>3</h1>`;
+                                setTimeout(() => {
+                                    document.querySelector(".container").innerHTML=`<h1>2</h1>`;
+                                    setTimeout(() => {
+                                        document.querySelector(".container").innerHTML=`<h1>1</h1>`;
+                                        setTimeout(() => {
+                                            document.querySelector(".container").innerHTML=`<h1>Happy Independence Day</h1>`;
+                                        }); // Callback Hell End
+                                    }, 1000); // Counter 1 End
+                                }, 1000); // Counter 2 End
+                            }, 1000); // Counter 3 End
+                        }, 1000); // Counter 4 End
+                    }, 1000); // Counter 5 End
+                }, 1000); // Counter 6 End
+            }, 1000); // Counter 7 End
+        }, 1000); // Counter 8 End
+    }, 1000); // Counter 9 End
+}, 1000); // Counter 10 End
